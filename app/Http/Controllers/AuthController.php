294bb4faf910ad30123f45e0cf837d6dcb758003
khaoula khaldi <khaoula2417@gmail.com>
2026-03-10 12:43:49 +0000

    ajouter controller de auth avec deux methode register et aussi connection

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,7 +20,28 @@ class AuthController extends Controller
             'password'->bcrypt($request->password)
         ]);
 
-        $token==user->createToken('auth_token')->pleinTextToken;
+        return response()->json($user);
+    }
 
+    public function login(){
+         $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:6' 
+        ]);
+
+        $user= User::where('email',$request->email)->first();
+
+        if(!$user || !Has::check($request->password,$user->password)){
+            return reponse()->json([
+                'message'=> 'data invalide'
+            ],401);
+        }
+        $token= $user->createToken('auth_token')->pleinTextToken;
+
+        return reponse()->json([
+            'user'=>$user,
+            'token'=>$token
+        ]);
+        
     }
 }
